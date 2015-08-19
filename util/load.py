f77be01f5c79498852512e1c2ab9bcaca0061ccc
Rene Nederhand <rene@cogfor.com>
2015-08-19 20:22:09 +0200

    cleaning up

--- util/load.py
+++ util/load.py
@@ -6,27 +6,27 @@ def one_hot(x,n):
     if type(x) == list:
         x = np.array(x)
     x = x.flatten()
-    o_h = np.zeros((len(x),n))
-    o_h[np.arange(len(x)),x] = 1
+    o_h = np.zeros((len(x), n))
+    o_h[np.arange(len(x)), x] = 1
     return o_h
 
 def mnist(ntrain=60000, ntest=10000, onehot=True):
     datasets_dir = load_config()['datafiles']['directory']
-    data_dir = os.path.join(datasets_dir,'mnist/')
-    fd = open(os.path.join(data_dir,'train-images-idx3-ubyte'))
+    data_dir = os.path.join(datasets_dir, 'mnist/')
+    fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
     loaded = np.fromfile(file=fd,dtype=np.uint8)
     trX = loaded[16:].reshape((60000,28*28)).astype(float)
 
-    fd = open(os.path.join(data_dir,'train-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
+    fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
     trY = loaded[8:].reshape((60000))
 
     fd = open(os.path.join(data_dir,'t10k-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
     teX = loaded[16:].reshape((10000,28*28)).astype(float)
 
-    fd = open(os.path.join(data_dir,'t10k-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
+    fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
     teY = loaded[8:].reshape((10000))
 
     trX = trX/255.
@@ -45,4 +45,4 @@ def mnist(ntrain=60000, ntest=10000, onehot=True):
         trY = np.asarray(trY)
         teY = np.asarray(teY)
 
-    return trX,teX,trY,teY
+    return trX, teX, trY, teY
